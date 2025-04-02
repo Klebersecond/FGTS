95b895dc1f7c898012788c859744c9bbe3ef14c6
Kleber Neves <klebernssii@gmail.com>
2025-04-02 20:38:37 -0300

    Finaliza progra FGTS

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double FGTS, salario;
+
+Console.Write("Digite o salário:");
+salario = Convert.ToDouble(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"valor do salario {salario}");
+Console.Write("valor do FGTS: ");
+
+//  Cálculo correto do FGTS (8% do salário)
+FGTS = salario * 0.08;
+Console.WriteLine($"{FGTS}");
+
+
+
+
+
